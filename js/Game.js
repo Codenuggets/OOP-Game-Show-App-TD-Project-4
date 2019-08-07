6a1a0759fad019d815d95c99f9bce282ff2db094
Codenuggets <wd14link@gmail.com>
2019-08-06 22:35:08 -0400

    Intialized files and created classes
    Starting classes and methods to Phrase.js and Game.js
    
    Added game start code to app.js

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+   constructor(missed, phrases, activePhrase) {
+     this.missed = 0;
+     this.phrases = [];
+     activePhrase = null;
+   }
+
+   function startGame() {
+
+   }
+
+   function getRandomPhrase() {
+
+   }
+
+   function handleInteraction() {
+
+   }
+
+   function removeLife() {
+
+   }
+
+   function checkForWin(){
+
+   }
+
+   function gameOver() {
+     
+   }
+ }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ const startGameBtn = document.querySelector('#btn_reset');
+ startGameBtn.addEventListener('click', () => {
+   const game = new Game();
+   game.startGame();
+ });
